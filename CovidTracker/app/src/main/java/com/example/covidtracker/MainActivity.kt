70d130bac4ff7aaeec96cfdcb786fc319b672c0d
Shashank Sinha <34626597+ShashankSinha98@users.noreply.github.com>
2020-08-02 00:46:49 +0530

    Top header added

--- CovidTracker/app/src/main/java/com/example/covidtracker/MainActivity.kt
+++ CovidTracker/app/src/main/java/com/example/covidtracker/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.covidtracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.Dispatcher
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +34,50 @@ class MainActivity : AppCompatActivity() {
             val response = withContext(Dispatchers.IO) { Client.api.execute() }
 
             if(response.isSuccessful){
-                Log.d(TAG+"_response",response.body?.string())
+                //Log.d(TAG+"_response",response.body?.string())
+                // ?. - Safe check, return null if null is present, else value
+                val data = Gson().fromJson(response.body?.string(),Response::class.java) // Converting Json array to list of state wise data
+
+                // Updating UI using - launch Dispatchers.Main
+                launch (Dispatchers.Main){
+                    // Displaying top header data
+                    bindCombinedData(data.statewise[0])
+                }
+            }
+        }
+    }
+
+    private fun bindCombinedData(data: StatewiseItem) {
+
+        val lastUpdatedTime : String? = data.lastupdatedtime
+        val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+        lastUpdatedTv.text = "Last Updated\n ${getTimeAgo(simpleDateFormat.parse(lastUpdatedTime))}"
+
+        confirmedTv.text = data.confirmed
+        recoveredTv.text = data.recovered
+        activeTv.text = data.active
+        deceasedTv.text = data.deaths
+
+    }
+
+    private fun getTimeAgo(past : Date) : String {
+        val now = Date() // Getting current date
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(now.time - past.time)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(now.time - past.time)
+        val hours = TimeUnit.MILLISECONDS.toHours(now.time-past.time)
+
+        return when {
+            seconds < 60 ->{
+                "Few Seconds ago"
+            }
+            minutes < 60 ->{
+                "$minutes minutes ago"
+            }
+            hours < 24 ->{
+                "$hours hour ${minutes % 60} min ago"
+            }
+            else ->{
+                SimpleDateFormat("dd/MM/yy, hh:mm a").format(past).toString()
             }
         }
     }
--- CovidTracker/app/src/main/java/com/example/covidtracker/Response.kt
+++ CovidTracker/app/src/main/java/com/example/covidtracker/Response.kt
@@ -1,2 +1,34 @@
 package com.example.covidtracker
 
+/*
+*   1. Data Class
+*   In Kotlin, you can create a data class to hold the data.
+*   The reason why would you want to mark a class as data is to let compiler know that you are creating
+*   this class for holding the data, compiler then creates several functions automatically for your data
+*   class which would be helpful in managing data.
+*
+*   2. ?= in Kotlin
+*   A variable of type String can not hold null. If we try to assign null to the variable, it gives compiler error.
+*   To allow a variable to hold null, we can declare a variable as nullable string, written String?
+*   eg- var s2: String? = "Robert"
+* */
+
+data class Response(
+    val statewise : List<StatewiseItem>
+)
+
+
+data class StatewiseItem(
+    val recovered : String?= null,
+    val deltadeaths : String?= null,
+    val deltarecovered : String?= null,
+    val active : String?= null,
+    val deltaconfirmed : String?= null,
+    val state : String?= null,
+    val statecode : String?= null,
+    val confirmed  : String?= null,
+    val deaths  : String?= null,
+    val lastupdatedtime : String?= null
+
+)
+
